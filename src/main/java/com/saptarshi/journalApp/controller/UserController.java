5d31fc9b3bbf059109f8535d388c47f184fb4eef
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2025-03-11 01:53:45 +0530

    general -u 11032025

--- src/main/java/com/saptarshi/journalApp/controller/UserController.java
+++ src/main/java/com/saptarshi/journalApp/controller/UserController.java
@@ -1,10 +1,39 @@
 package com.saptarshi.journalApp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.saptarshi.journalApp.entity.User;
+import com.saptarshi.journalApp.service.UserService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
 
+    @GetMapping
+    public List<User> getAllUser() {
+        return userService.getAll();
+    }
+
+    @PostMapping
+    public void createUser(@RequestBody User user) {
+        userService.saveEntry(user);
+    }
+
+    @PutMapping("/{userName}")
+    public ResponseEntity<?> updateUser (@RequestBody User user, @PathVariable String userName) {
+        User userInDb = userService.findByUserName(userName);
+        if(userInDb != null) {
+            userInDb.setUserName(user.getUserName());
+            userInDb.setPassword(user.getPassword());
+            userService.saveEntry(userInDb);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/com/saptarshi/journalApp/repository/UserRepository.java
+++ src/main/java/com/saptarshi/journalApp/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.saptarshi.journalApp.repository;
 
+import com.saptarshi.journalApp.entity.User;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<UserRepository, ObjectId> {
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    User findByUserName(String username);
 }
