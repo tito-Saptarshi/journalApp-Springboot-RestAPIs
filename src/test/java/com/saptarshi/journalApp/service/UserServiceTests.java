e72a99791f5e66cda53706e74c8623189f2ce43f
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2025-03-23 21:33:41 +0530

    JUnit -u 0.1 23032025

---
+++ src/test/java/com/saptarshi/journalApp/service/UserServiceTests.java
@@ -0,0 +1,11 @@
+package com.saptarshi.journalApp.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class UserServiceTests {
+
+    @Test
+    public void  testAdd() {
+        assertEquals(4, 2+2);
+    }
+}
